a6991f4241d86c12b81324abe4d41852b4ddc923
Baggsy <a.panteli.tue@gmail.com>
2018-02-09 16:53:46 +0100

    fixing the renaming

--- DraughtsPlugin/src/nl/tue/s2id90/group50/Bot1.java
+++ DraughtsPlugin/src/nl/tue/s2id90/group50/Bot1.java
@@ -14,14 +14,14 @@ import org10x10.dam.game.Move;
  */
 // ToDo: rename this class (and hence this file) to have a distinct name
 //       for your player during the tournament
-public class MyDraughtsPlayer  extends DraughtsPlayer{
+public class Bot1  extends DraughtsPlayer{
     private int bestValue=0;
     int maxSearchDepth;
     
     /** boolean that indicates that the GUI asked the player to stop thinking. */
     private boolean stopped;
 
-    public MyDraughtsPlayer(int maxSearchDepth) {
+    public Bot1(int maxSearchDepth) {
         super("best.png"); // ToDo: replace with your own icon
         this.maxSearchDepth = maxSearchDepth;
     }
