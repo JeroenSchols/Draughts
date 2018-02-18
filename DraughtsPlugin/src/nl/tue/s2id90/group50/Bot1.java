8c031da732334a4acf365c1a7025a93b63d47ac9
s161530 <s161530@s161530.campus.tue.nl>
2018-02-18 14:59:41 +0100

    Try at implementing Alpha-Beta (Might have ruined project setup)

--- DraughtsPlugin/src/nl/tue/s2id90/group50/Bot1.java
+++ DraughtsPlugin/src/nl/tue/s2id90/group50/Bot1.java
@@ -10,130 +10,186 @@ import org10x10.dam.game.Move;
 
 /**
  * Implementation of the DraughtsPlayer interface.
+ *
  * @author huub
  */
 // ToDo: rename this class (and hence this file) to have a distinct name
 //       for your player during the tournament
-public class Bot1  extends DraughtsPlayer{
-    private int bestValue=0;
+public class Bot1 extends DraughtsPlayer {
+
+    private int bestValue = 0;
     int maxSearchDepth;
-    
-    /** boolean that indicates that the GUI asked the player to stop thinking. */
+
+    /**
+     * boolean that indicates that the GUI asked the player to stop thinking.
+     */
     private boolean stopped;
 
     public Bot1(int maxSearchDepth) {
         super("best.png"); // ToDo: replace with your own icon
         this.maxSearchDepth = maxSearchDepth;
     }
-    
-    @Override public Move getMove(DraughtsState s) {
+
+    @Override
+    public Move getMove(DraughtsState s) {
         Move bestMove = null;
         bestValue = 0;
         DraughtsNode node = new DraughtsNode(s);    // the root of the search tree
         try {
             // compute bestMove and bestValue in a call to alphabeta
             bestValue = alphaBeta(node, MIN_VALUE, MAX_VALUE, maxSearchDepth);
-            
+
             // store the bestMove found uptill now
             // NB this is not done in case of an AIStoppedException in alphaBeat()
-            bestMove  = node.getBestMove();
-            
+            bestMove = node.getBestMove();
+
             // print the results for debugging reasons
             System.err.format(
-                "%s: depth= %2d, best move = %5s, value=%d\n", 
-                this.getClass().getSimpleName(),maxSearchDepth, bestMove, bestValue
+                    "%s: depth= %2d, best move = %5s, value=%d\n",
+                    this.getClass().getSimpleName(), maxSearchDepth, bestMove, bestValue
             );
-        } catch (AIStoppedException ex) {  /* nothing to do */  }
-        
-        if (bestMove==null) {
+        } catch (AIStoppedException ex) { /* nothing to do */ }
+
+        if (bestMove == null) {
             System.err.println("no valid move found!");
             return getRandomValidMove(s);
         } else {
             return bestMove;
         }
-    } 
+    }
 
-    /** This method's return value is displayed in the AICompetition GUI.
-     * 
-     * @return the value for the draughts state s as it is computed in a call to getMove(s). 
+    /**
+     * This method's return value is displayed in the AICompetition GUI.
+     *
+     * @return the value for the draughts state s as it is computed in a call to getMove(s).
      */
-    @Override public Integer getValue() { 
-       return bestValue;
+    @Override
+    public Integer getValue() {
+        return bestValue;
     }
 
-    /** Tries to make alphabeta search stop. Search should be implemented such that it
-     * throws an AIStoppedException when boolean stopped is set to true;
-    **/
-    @Override public void stop() {
-       stopped = true; 
+    /**
+     * Tries to make alphabeta search stop. Search should be implemented such that it throws an AIStoppedException when
+     * boolean stopped is set to true;
+     *
+     */
+    @Override
+    public void stop() {
+        stopped = true;
     }
-    
-    /** returns random valid move in state s, or null if no moves exist. */
+
+    /**
+     * returns random valid move in state s, or null if no moves exist.
+     */
     Move getRandomValidMove(DraughtsState s) {
         List<Move> moves = s.getMoves();
         Collections.shuffle(moves);
-        return moves.isEmpty()? null : moves.get(0);
+        return moves.isEmpty() ? null : moves.get(0);
     }
-    
-    /** Implementation of alphabeta that automatically chooses the white player
-     *  as maximizing player and the black player as minimizing player.
+
+    /**
+     * Implementation of alphabeta that automatically chooses the white player as maximizing player and the black player
+     * as minimizing player.
+     *
      * @param node contains DraughtsState and has field to which the best move can be assigned.
      * @param alpha
      * @param beta
      * @param depth maximum recursion Depth
      * @return the computed value of this node
      * @throws AIStoppedException
-     **/
-    int alphaBeta(DraughtsNode node, int alpha, int beta, int depth)
-            throws AIStoppedException
-    {
+     *
+     */
+    int alphaBeta(DraughtsNode node, int alpha, int beta, int depth) throws AIStoppedException {
         if (node.getState().isWhiteToMove()) {
             return alphaBetaMax(node, alpha, beta, depth);
-        } else  {
+        } else {
             return alphaBetaMin(node, alpha, beta, depth);
         }
     }
-    
-    /** Does an alphabeta computation with the given alpha and beta
-     * where the player that is to move in node is the minimizing player.
-     * 
-     * <p>Typical pieces of code used in this method are:
-     *     <ul> <li><code>DraughtsState state = node.getState()</code>.</li>
-     *          <li><code> state.doMove(move); .... ; state.undoMove(move);</code></li>
-     *          <li><code>node.setBestMove(bestMove);</code></li>
-     *          <li><code>if(stopped) { stopped=false; throw new AIStoppedException(); }</code></li>
-     *     </ul>
+
+    /**
+     * Does an alphabeta computation with the given alpha and beta where the player that is to move in node is the
+     * minimizing player.
+     *
+     * <p>
+     * Typical pieces of code used in this method are:
+     * <ul> <li><code>DraughtsState state = node.getState()</code>.</li>
+     * <li><code> state.doMove(move); .... ; state.undoMove(move);</code></li>
+     * <li><code>node.setBestMove(bestMove);</code></li>
+     * <li><code>if(stopped) { stopped=false; throw new AIStoppedException(); }</code></li>
+     * </ul>
      * </p>
+     *
      * @param node contains DraughtsState and has field to which the best move can be assigned.
      * @param alpha
      * @param beta
-     * @param depth  maximum recursion Depth
+     * @param depth maximum recursion Depth
      * @return the compute value of this node
      * @throws AIStoppedException thrown whenever the boolean stopped has been set to true.
      */
-     int alphaBetaMin(DraughtsNode node, int alpha, int beta, int depth)
-            throws AIStoppedException {
-        if (stopped) { stopped = false; throw new AIStoppedException(); }
+    int alphaBetaMin(DraughtsNode node, int alpha, int beta, int depth) throws AIStoppedException {
+        if (stopped) {
+            stopped = false;
+            throw new AIStoppedException();
+        }
         DraughtsState state = node.getState();
         // ToDo: write an alphabeta search to compute bestMove and value
+        if (depth < 0) {
+            return evaluate(state);
+        }
+        List<Move> possibleMoves = state.getMoves();
         Move bestMove = state.getMoves().get(0);
-        int value = 0;
-        node.setBestMove(bestMove);
-        return value;
-     }
-    
-    int alphaBetaMax(DraughtsNode node, int alpha, int beta, int depth)
-            throws AIStoppedException {
-        if (stopped) { stopped = false; throw new AIStoppedException(); }
-        DraughtsState state = node.getState();
-        // ToDo: write an alphabeta search to compute bestMove and value
-        Move bestMove = state.getMoves().get(0);
+        for (Move move : possibleMoves) {
+            state.doMove(move);
+            int foundBeta = alphaBetaMax(new DraughtsNode(state), alpha, beta, depth - 1);
+            if (beta > foundBeta) {
+                bestMove = move;
+                beta = foundBeta;
+                if (beta <= alpha) {
+                    return alpha;
+                }
+            }
+            state.undoMove(move);
+        }
         int value = 0;
         node.setBestMove(bestMove);
         return value;
     }
 
-    /** A method that evaluates the given state. */
+    int alphaBetaMax(DraughtsNode node, int alpha, int beta, int depth) throws AIStoppedException {
+        if (stopped) {
+            stopped = false;
+            throw new AIStoppedException();
+        }
+        DraughtsState state = node.getState();
+        // ToDo: write an alphabeta search to compute bestMove and value
+        if (depth < 0) {
+            return evaluate(state);
+        }
+        List<Move> possibleMoves = state.getMoves();
+        Move bestMove = state.getMoves().get(0);
+        for (Move move : possibleMoves) {
+            state.doMove(move);
+            int foundAlpha = alphaBetaMin(new DraughtsNode(state), alpha, beta, depth - 1);
+            if (alpha < foundAlpha) {
+                bestMove = move;
+                alpha = foundAlpha;
+                if (alpha >= beta) {
+                    return beta;
+                }
+            }
+            state.undoMove(move);
+        }
+        int value = 0;
+        node.setBestMove(bestMove);
+        return value;
+    }
+
+    /**
+     * A method that evaluates the given state.
+     */
     // ToDo: write an appropriate evaluation function
-    int evaluate(DraughtsState state) { return 0; }
+    int evaluate(DraughtsState state) {
+        return 0;
+    }
 }
